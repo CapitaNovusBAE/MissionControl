0992be4888244e63e4b47429456236f67e98f9fe
James Paul <Novus_BAE@Novus6.lan>
2016-04-04 09:21:40 +0100

    added missing files

---
+++ MissionControl/src/view/admin/AdminPageController.java
@@ -0,0 +1,18 @@
+package view.admin;
+
+import controller.MainApp;
+
+public class AdminPageController {
+
+	@SuppressWarnings("unused")
+	private MainApp mainApp;
+
+
+
+
+
+	public void setMainApp(MainApp mainApp) {
+		// TODO Auto-generated method stub
+		this.mainApp = mainApp;
+	}
+}
---
+++ MissionControl/src/view/home/HomeController.java
@@ -0,0 +1,42 @@
+package view.home;
+
+import controller.MainApp;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class HomeController {
+
+	@FXML
+	Button home;
+	@FXML
+	Button assign;
+	@FXML
+	Button review;
+	@FXML
+	Button admin;
+
+	private MainApp mainApp;
+
+	@FXML
+    private void buttonPress(ActionEvent event){
+
+    	if(event.getSource().equals(home)) {
+    		//***********************
+    		//***********************
+    	} else if (event.getSource().equals(assign)){
+    		//***********************
+    		//***********************
+    	} else if (event.getSource().equals(review)){
+    		//***********************
+    		//***********************
+    	} else if (event.getSource().equals(admin)){
+    		mainApp.adminPage();
+    	}
+    }
+
+	public void setMainApp(MainApp mainApp) {
+		// TODO Auto-generated method stub
+		this.mainApp = mainApp;
+	}
+}
