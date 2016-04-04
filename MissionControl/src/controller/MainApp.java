0e5fa6f92f705b4f1cdee24e96705485bcacb7bf
Vadim <vadim@hotmail.co.uk>
2016-04-04 17:44:27 +0100

    assign page

--- MissionControl/src/controller/MainApp.java
+++ MissionControl/src/controller/MainApp.java
@@ -7,203 +7,168 @@ package controller;
 
 import java.io.IOException;
 
+import dao.user.User;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-import view.home.HomePageController;
 import view.admin.AdminPageController;
+import view.home.HomePageController;
 import view.layout.LayoutController;
 import view.navigation.NavigationMenuController;
 import view.signin.SignInController;
 
 public class MainApp extends Application {
 
-    private Stage primaryStage, signInStage;
-    private AnchorPane signInLayout;
-    private BorderPane mainLayout;
+	private Stage primaryStage, signInStage;
+	private AnchorPane signInLayout;
+	private BorderPane mainLayout;
+	private User user;
+	//SET STAGES AND INITIALIZE SIGN IN
+	@Override
+	public void start(final Stage primaryStage) {
 
-    //SET STAGES AND INITIALIZE SIGN IN
-    @Override
-    public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("APPLICATION NAME TBC");
 
-        this.primaryStage = primaryStage;
-        this.primaryStage.setTitle("APPLICATION NAME TBC");
+		this.signInStage = new Stage();
+		this.signInStage.setTitle("Sign In");
 
-        this.signInStage = new Stage();
-        this.signInStage.setTitle("Sign In");
+		SignIn();
+	}
 
-        SignIn();
-    }
-
-    //INITIALIZE SIGN IN STAGE
-    public void SignIn(){
-    	try{
-    		//LOAD FXML
-    		FXMLLoader loader = new FXMLLoader();
-    		loader.setLocation(MainApp.class.getResource("/view/signin/SignIn.fxml"));
-    		signInLayout = (AnchorPane) loader.load();
-
-    		//CONTROLLER
-    		SignInController controller = loader.getController();
-    		controller.setMainApp(this);
-
-    		//SET SIGN IN SCENE
-    		Scene scene = new Scene(signInLayout);
-    		signInStage.setScene(scene);
-    		signInStage.show();
-
-    	} catch (IOException e) {
-    		e.printStackTrace();
-    	}
-    }
-
-    //INITIALIZE PRIMARY STAGE LAYOUT
-    public void initLayout() {
-        try {
-        	//HIDE SIGN IN WINDOW
-        	signInStage.hide();
-
-        	//LOAD FXML
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("/view/layout/Layout.fxml"));
-            mainLayout = (BorderPane) loader.load();
-
-            //CONTROLLER
-            LayoutController controller = loader.getController();
-    		controller.setMainApp(this);
-
-    		//SET MAIN SCENE
-            Scene scene = new Scene(mainLayout);
-            primaryStage.setScene(scene);
-            primaryStage.show();
-
-            navigationMenu();
-            homePage();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void navigationMenu(){
-    	try{
-    	//LOAD FXML
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(MainApp.class.getResource("/view/navigation/NavigationMenu.fxml"));
-        AnchorPane navigationMenu = (AnchorPane) loader.load();
-
-        //CONTROLLER
-        NavigationMenuController controller = loader.getController();
-		controller.setMainApp(this);
-
-		//PLACE IN SCENE
-		mainLayout.setLeft(navigationMenu);
-
-    	} catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void homePage(){
-    	try{
-        	//LOAD FXML
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("/view/home/HomePage.fxml"));
-            AnchorPane homePage = (AnchorPane) loader.load();
-
-            //CONTROLLER
-            HomePageController controller = loader.getController();
-    		controller.setMainApp(this);
-
-    		//PLACE IN SCENE
-    		mainLayout.setCenter(homePage);
-
-        	} catch (IOException e) {
-                e.printStackTrace();
-            }
-    }
-
-	public void adminPage(){
+	//INITIALIZE SIGN IN STAGE
+	public void SignIn(){
 		try{
 			//LOAD FXML
-			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(MainApp.class.getResource("/view/admin/AdminPage.fxml"));
-			AnchorPane adminPage = (AnchorPane) loader.load();
+			final FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("/view/signin/SignIn.fxml"));
+			this.signInLayout = (AnchorPane) loader.load();
 
 			//CONTROLLER
-			AdminPageController controller = loader.getController();
-    		controller.setMainApp(this);
+			final SignInController controller = loader.getController();
+			controller.setMainApp(this);
 
-    		//PLACE IN SCENE
-    		mainLayout.setCenter(adminPage);
+			//SET SIGN IN SCENE
+			final Scene scene = new Scene(this.signInLayout);
+			this.signInStage.setScene(scene);
+			this.signInStage.show();
 
-		} catch (IOException e) {
+		} catch (final IOException e) {
 			e.printStackTrace();
 		}
 	}
 
+	//INITIALIZE PRIMARY STAGE LAYOUT
+	public void initLayout() {
+		try {
+			//HIDE SIGN IN WINDOW
+			this.signInStage.hide();
+
+			//LOAD FXML
+			final FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("/view/layout/Layout.fxml"));
+			this.mainLayout = (BorderPane) loader.load();
+
+			//CONTROLLER
+			final LayoutController controller = loader.getController();
+			controller.setMainApp(this);
+
+			//SET MAIN SCENE
+			final Scene scene = new Scene(this.mainLayout);
+			this.primaryStage.setScene(scene);
+			this.primaryStage.show();
+
+			navigationMenu();
+			homePage();
+
+		} catch (final IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void navigationMenu(){
+		try{
+			//LOAD FXML
+			final FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("/view/navigation/NavigationMenu.fxml"));
+			final AnchorPane navigationMenu = (AnchorPane) loader.load();
+
+			//CONTROLLER
+			final NavigationMenuController controller = loader.getController();
+			controller.setMainApp(this);
+
+			//PLACE IN SCENE
+			this.mainLayout.setLeft(navigationMenu);
+
+		} catch (final IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void homePage(){
+		try{
+			//LOAD FXML
+			final FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("/view/home/HomePage.fxml"));
+			final AnchorPane homePage = (AnchorPane) loader.load();
+
+			//CONTROLLER
+			final HomePageController controller = loader.getController();
+			controller.setMainApp(this);
+
+			//PLACE IN SCENE
+			this.mainLayout.setCenter(homePage);
+
+		} catch (final IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void adminPage(){
+		try{
+			//LOAD FXML
+			final FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("/view/admin/AdminPage.fxml"));
+			final AnchorPane adminPage = (AnchorPane) loader.load();
+
+			//CONTROLLER
+			final AdminPageController controller = loader.getController();
+			controller.setMainApp(this);
+
+			//PLACE IN SCENE
+			this.mainLayout.setCenter(adminPage);
+
+		} catch (final IOException e) {
+			e.printStackTrace();
+		}
+	}
+	public void assignPage(){
+		System.out.println("Assign page selected");
+		try{
+			final FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("/view/assign/AssignPage.fxml"));
+			final AnchorPane page = (AnchorPane) loader.load();
+
+			this.mainLayout.setBottom(page);
+
+		} catch (final IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public User getUser(){
+		return this.user;
+	}
 
 
+	public Stage getPrimaryStage() {
+		return this.primaryStage;
+	}
 
-
-
-/*
-    public void showMap(){
-    	try{
-    		// Load root layout from fxml file.
-    		FXMLLoader loader = new FXMLLoader();
-    		loader.setLocation(MainApp.class.getResource("/view/WaypointList.fxml"));
-    		AnchorPane Map = (AnchorPane) loader.load();
-
-            gridPane.add(Map, 1, 0);
-    	} catch (IOException e) {
-    		e.printStackTrace();
-    	}
-    }
-
-    public void showWaypointList(){
-    	try{
-    		// Load root layout from fxml file.
-    		FXMLLoader loader = new FXMLLoader();
-    		loader.setLocation(MainApp.class.getResource("/view/Map.fxml"));
-    		AnchorPane waypointList = (AnchorPane) loader.load();
-
-            gridPane.add(waypointList, 0, 0);
-
-//          Give the controller access to the main app.
-//          PersonOverviewController controller = loader.getController();
-//          controller.setMainApp(this);
-
-    	} catch (IOException e) {
-    		e.printStackTrace();
-    	}
-    }
-
-    public void showWaypointPlacement(){
-    	try{
-    		// Load root layout from fxml file.
-    		FXMLLoader loader = new FXMLLoader();
-    		loader.setLocation(MainApp.class.getResource("/view/WaypointPlacement.fxml"));
-    		AnchorPane waypointPlacement = (AnchorPane) loader.load();
-
-            gridPane.add(waypointPlacement, 0, 1);
-    	} catch (IOException e) {
-    		e.printStackTrace();
-    	}
-    }
-
-    //NEED TO MERGE CELLS 0/1 AND 1/1 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-*/
-
-
-    public Stage getPrimaryStage() {
-        return primaryStage;
-    }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+	public static void main(final String[] args) {
+		launch(args);
+	}
 }
---
+++ MissionControl/src/view/assign/flightpath/FlightPathController.java
@@ -0,0 +1,93 @@
+package view.assign.flightpath;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javax.swing.JPanel;
+
+import controller.MainApp;
+import dao.mission.Mission;
+import gov.nasa.worldwind.BasicModel;
+import gov.nasa.worldwind.awt.WorldWindowGLCanvas;
+import gov.nasa.worldwind.geom.LatLon;
+import gov.nasa.worldwind.geom.Position;
+import javafx.collections.ObservableList;
+import javafx.embed.swing.SwingNode;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+/**
+ * @author Controller for
+ *
+ */
+public class FlightPathController implements Initializable{
+
+	@FXML
+	private ListView<Position> positionsView;;
+
+	@FXML
+	private SwingNode mapView;
+
+	@FXML
+	private TextField latitude;
+
+	@FXML
+	private TextField longtitude;
+
+	@FXML
+	private TextField elevation;
+
+	@FXML
+	private Button addWaypointButton;;
+
+
+	private MainApp mainApp;
+
+	private Mission mission;
+
+
+	public void setMainApp(final MainApp mainApp) {
+		this.mainApp = mainApp;
+	}
+
+	@Override
+	public void initialize(final URL location, final ResourceBundle resources) {
+		this.mission = new Mission("user", "Untitled");
+	}
+
+	/**
+	 * Action on add waypoint button click
+	 */
+	public void addWaypoint(){
+		this.mission.addPositions(new Position(LatLon.fromDegrees(Double.parseDouble(this.latitude.getText()),Double.parseDouble(this.longtitude.getText())),Double.parseDouble(this.elevation.getText())));
+		this.latitude.clear();
+		this.longtitude.clear();
+		this.elevation.clear();
+		updateView();
+	}
+
+	private void updateView(){
+		final ObservableList<Position> positions = this.positionsView.getItems();
+		positions.clear();
+		positions.addAll(this.mission.getPositions());
+	}
+
+	private JPanel getMap(){
+
+		//create a WorldWind main object
+//		final WorldWindowGLCanvas worldWindCanvas = new WorldWindowGLCanvas();
+//		worldWindCanvas.setModel(new BasicModel());
+//
+//		//build Java swing interface
+		final JPanel panel = new JPanel();
+//		panel.add(worldWindCanvas);
+//		panel.setSize(800,600);
+//		panel.setVisible(true);
+
+			return panel;
+	}
+
+}
